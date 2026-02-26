212e2916d6828c428d25655da5e6cc54297f15f8
VeerRana01 <1000021449@dit.edu.in>
2026-02-26 21:27:14 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,13 +1,14 @@
 let products = [];
 
+function loadProductsFromJSON(callback){
+
 fetch("products/products.json")
 .then(res => res.json())
 .then(data => {
 
 products = data;
 
-if(typeof loadProducts === "function")
-loadProducts();
+if(callback) callback();
 
 });
-
+}
